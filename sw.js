e14dd7c50bfb82f6d5a9d3a21380adbf4c2f3183
soorar421 <146207467+soorar421@users.noreply.github.com>
2026-05-04 19:58:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,37 @@
-const CACHE_NAME = 'gs-const-v1';
-const ASSETS = [
+const CACHE_NAME = 'gs-constructions-v1';
+const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-// ഇൻസ്റ്റാൾ ചെയ്യുമ്പോൾ ഫയലുകൾ ക്യാഷ് ചെയ്യുന്നു
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+// Install Event - Caching basic assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      console.log('Service Worker: Caching Files');
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
-  console.log('Service Worker Installed');
 });
 
-// ആപ്പ് വർക്ക് ചെയ്യാൻ ഇത് അത്യാവശ്യമാണ്
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+// Activate Event - Cleaning up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event - Serving files from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
