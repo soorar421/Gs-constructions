2b599b9054d542ef56c2d99cc52fe6986c38e9e1
soorar421 <146207467+soorar421@users.noreply.github.com>
2026-05-04 20:16:39 +0530

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,37 +1,19 @@
-const CACHE_NAME = 'gs-constructions-v1';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'gs-constructions-v2';
+const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+  './manifest.json'
 ];
 
-// Install Event - Caching basic assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Service Worker: Caching Files');
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activate Event - Cleaning up old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Fetch Event - Serving files from cache when offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
+
