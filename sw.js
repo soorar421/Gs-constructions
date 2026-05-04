167b13b134f9bec841b273c6fcbe5ffa4babce37
soorar421 <146207467+soorar421@users.noreply.github.com>
2026-05-04 19:52:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
+const CACHE_NAME = 'gs-const-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// ഇൻസ്റ്റാൾ ചെയ്യുമ്പോൾ ഫയലുകൾ ക്യാഷ് ചെയ്യുന്നു
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
   console.log('Service Worker Installed');
 });
 
+// ആപ്പ് വർക്ക് ചെയ്യാൻ ഇത് അത്യാവശ്യമാണ്
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
