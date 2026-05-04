9281cb505b924b45c8ad0c8a94f666e40166a167
soorar421 <146207467+soorar421@users.noreply.github.com>
2026-05-04 22:47:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,27 @@
-const CACHE_NAME = 'gs-site-manager-v2';
+const CACHE_NAME = 'gs-constructions-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './icon.png',
-                './manifest.json'
-            ]);
-        })
-    );
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+// Fetch events
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
